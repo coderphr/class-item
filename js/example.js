4b254635f000315d7e117c8ba3951213604bf5d3
coderphr <qq1730684350@163.com>
2020-09-10 19:30:57 +0800

    Our Products

--- js/example.js
+++ js/example.js
@@ -1,3 +1,12 @@
 $(function() {
-	$('.flexslider').flexslider()
+	$('.flexslider').flexslider();
+	$(".box").hover(function(){
+		$(this).find('.iconfont').addClass("animated fadeInDown");
+		$(this).find('p').addClass('animated fadeInUp');
+	},
+	function(){
+		
+	$(this).find('.iconfont').removeClass('animated fadeInDown');
+	$(this).find('p').removeClass('animated fadeInUp');
+	})
 })
